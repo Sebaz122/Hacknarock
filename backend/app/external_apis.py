e272f08ce6d7b8956e44c19b7295cb50399cda3e
Sebaz122 <szarebski@student.agh.edu.pl>
2025-04-12 16:07:57 +0200

    Small changes to genre function

--- backend/app/external_apis.py
+++ backend/app/external_apis.py
@@ -19,7 +19,7 @@ def _get_genres(song):
 def prepare_songs_and_genres():
     return _get_random_genre()
 
-def get_new_token():
+def _get_new_token():
     auth_string = client_id+":"+client_secret
     auth_bytes=auth_string.encode("utf-8")
     auth_base64=str(base64.b64encode(auth_bytes), "utf-8")
@@ -32,27 +32,40 @@ def get_new_token():
     data={"grant_type": "client_credentials"}
     result=post(url,headers=headers, data=data)
     json_result=json.loads(result.content)
-    # print(json_result)
     token=json_result["access_token"]
     return token
 
-def get_auth_header(token):
+def _get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
-def search_for_genre(token, genre):
+def _get_genre_songs_urls(token, genre):
     url = "https://api.spotify.com/v1/search"
-    headers= get_auth_header(token)
-    query= f"q={genre}&type=track&limit=9"
+    headers= _get_auth_header(token)
+    query= f"q={genre}&type=track&limit=20"
     query_url = url+"?"+query
     result=get(query_url, headers=headers)
     json_result=json.loads(result.content)
-    print(json_result)
-    # for i in json_result:
-    #     print(i)
+    track_urls=[]
+    track_names=[]
+    max=0
+    for track in json_result['tracks']['items']:
+        track_url=track['external_urls']['spotify']
+        if(track_url):
+            track_urls.append(track_url)
+            track_name=track['name']
+            track_names.append(track_name)
+            max+=1
+            if max==9:
+                break
+        else:
+            continue
+    print(track_urls)
+    print(track_names)
+
 
 if __name__=="__main__":
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     client_secret= os.getenv("CLIENT_SECRET")
-    token=get_new_token()
-    search_for_genre(token, "rock")
+    token=_get_new_token()
+    _get_genre_songs_urls(token, "rock")
