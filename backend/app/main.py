4be007493e98ad14fff078d8e7429bd55b8553eb
Szymon Wojturski <wojtursk@student.agh.edu.pl>
2025-04-13 01:18:19 +0200

    updated in memory db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from email.policy import default
 
 from pydantic import BaseModel
@@ -5,30 +6,34 @@ from fastapi import FastAPI,status
 from fastapi.responses import JSONResponse
 
 app=FastAPI()
-users={}
+streak={}
+date={}
 
 class User(BaseModel):
     id:str
     streak:int
+    last_update:datetime
 
 @app.get("/")
 async def root() :
     return {"message" : "Hello World"}
 
 @app.get("/streak/increment")
-async def hardcoded_example(user_id:str):
-    if not user_id in users:
-        users[user_id]=0
-    users[user_id]+=1
-    return JSONResponse(status_code=status.HTTP_200_OK)
+async def increment_streak(user_id:str,current_date:datetime):
+    if not user_id in streak:
+        streak[user_id]=0
+    streak[user_id]+=1
+    date[user_id]=current_date
+    return JSONResponse(status_code=status.HTTP_200_OK,content="ok")
 
 @app.get("/streak/reset")
-async def hardcoded_example(user_id:str):
-    if not user_id in users:
-        users[user_id]=0
-    users[user_id] = 0
-    return JSONResponse(status_code=status.HTTP_200_OK)
+async def reset_streak(user_id:str,current_date:datetime):
+    if not user_id in streak:
+        streak[user_id]=0
+    streak[user_id] = 0
+    date[user_id]=current_date
+    return JSONResponse(status_code=status.HTTP_200_OK,content="ok")
 
 @app.get("/streak/get")
-async def hardcoded_example(user_id:str):
-    return {"Streak":users[user_id]}
+async def get_streak(user_id:str):
+    return {"streak":streak[user_id],"lastUpdate":date[user_id]}
