26401e0bd4a8b5919c92b1c4e123dce9d7736301
Szymon Wojturski <wojtursk@student.agh.edu.pl>
2025-04-12 23:51:48 +0200

    basic in memory db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,34 @@
 from email.policy import default
 
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI,status
+from fastapi.responses import JSONResponse
 
 app=FastAPI()
+users={}
+
+class User(BaseModel):
+    id:str
+    streak:int
 
 @app.get("/")
 async def root() :
     return {"message" : "Hello World"}
 
-@app.get("/hardcoded")
-async def hardcoded_example():
-    return {""}
+@app.get("/streak/increment")
+async def hardcoded_example(user_id:str):
+    if not user_id in users:
+        users[user_id]=0
+    users[user_id]+=1
+    return JSONResponse(status_code=status.HTTP_200_OK)
 
+@app.get("/streak/reset")
+async def hardcoded_example(user_id:str):
+    if not user_id in users:
+        users[user_id]=0
+    users[user_id] = 0
+    return JSONResponse(status_code=status.HTTP_200_OK)
 
-@app.get("/daily")
-async def hardcoded_example():
-    return {""}
-
-@app.get("/random")
-async def daily():
-    return {""}
+@app.get("/streak/get")
+async def hardcoded_example(user_id:str):
+    return {"Streak":users[user_id]}
