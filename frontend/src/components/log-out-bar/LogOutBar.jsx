29a9fd8892193333a92ce15f57555e68f0ce5c0c
MMRasbb <maciek.malinowski.poczta@gmail.com>
2025-04-13 06:15:57 +0200

    hjusxx

--- frontend/src/components/log-out-bar/LogOutBar.jsx
+++ frontend/src/components/log-out-bar/LogOutBar.jsx
@@ -1,11 +1,16 @@
 import {Box, Button, Typography} from "@mui/material";
 import "./LogOutBar.scss"
+import {useStore} from "../../store.js";
 
 function LogOutBar({name}) {
+    const handleLogOut = async () => {
+        useStore.getState().setToken(null);
+    }
+
     return (
         <Box className="log-out-bar">
             <Typography className="text"> You're logged in as {name}</Typography>
-            <Button className="log-out-button">Logout</Button>
+            <Button className="log-out-button" onClick={handleLogOut}>Logout</Button>
         </Box>
     )
 }
